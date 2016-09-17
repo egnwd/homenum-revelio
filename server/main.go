0245949845db97421b02ac17ee2fe71d2deabfd8
Elliot Greenwood <hello@elliotgreenwood.co.uk>
2016-09-17 13:44:51 +0100

    Add logging and flags

--- server/main.go
+++ server/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	r := mux.NewRouter()
-	static := "./client"
-	r.Handle("/", WhoIsHomeHandler(static))
+type config struct {
+	static string
+	port   int
+}
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+func main() {
+	log.SetPrefix("Homenum Revelio: ")
+	c := parseArgs()
+
+	r := mux.NewRouter()
+	r.Handle("/", WhoIsHomeHandler(c.static))
+
+	log.Println("Listening on localhost:", c.port)
+	port := fmt.Sprintf(":%d", c.port)
+	log.Fatal(http.ListenAndServe(port, r))
 }
 
 // WhoIsHomeHandler displays a list of people on the network
@@ -20,4 +31,13 @@ func WhoIsHomeHandler(static string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, static+"/index.html")
 	})
+}
+
+func parseArgs() (c *config) {
+	c = new(config)
+	flag.IntVar(&c.port, "p", 8080, "Port for the server")
+	flag.StringVar(&c.static, "static", "./client", "")
+	flag.Parse()
+
+	return
 }
